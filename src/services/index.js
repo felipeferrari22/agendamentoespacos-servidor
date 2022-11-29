50940bc43e37f073aeb5893b9c4f8701a028c4e5
FerrisOne <feferraripetruzza@gmail.com>
2022-11-29 13:14:22 -0300

    [New] Criação de Middlewares e Services

---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+const AuthPwd = require('./AuthPwd')
+const HashPwd = require('./HashPwd')
+const SetExpDate = require('./SetExpDate')
+
+module.exports = {
+    AuthPwd,
+    HashPwd,
+    SetExpDate
+}
