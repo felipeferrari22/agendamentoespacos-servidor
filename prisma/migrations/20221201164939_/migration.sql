2669d0fb04efde2415295792049ddce9c4d576e6
FerrisOne <feferraripetruzza@gmail.com>
2022-12-01 14:31:05 -0300

    [New] Adiciona todas as outras funcionalidades

---
+++ prisma/migrations/20221201164939_/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE `solicitacao` DROP FOREIGN KEY `solicitacao_espaco_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `solicitacao` DROP FOREIGN KEY `solicitacao_usuario_id_fkey`;
